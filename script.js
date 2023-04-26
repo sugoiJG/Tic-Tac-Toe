0eab33f7128e8f1551c5765a99f4c2f0a3f2b13b
sugoiJG <jasongolliet@icloud.com>
2023-04-26 15:04:06 -0700

    added Xs and Os, finished win condition and tie condition

--- script.js
+++ script.js
@@ -1,9 +1,130 @@
+const App = {
+  // all our selected html elements
+  $: {
+    menu: document.querySelector("[data-id='menu']"),
+    menuItems: document.querySelector("[data-id='menu-items']"),
+    resetBtn: document.querySelector("[data-id='reset-btn']"),
+    newRoundBtn: document.querySelector("[data-id='new-round-btn']"),
+    squares: document.querySelectorAll("[data-id='square']"),
+    modal: document.querySelector("[data-id='modal']"),
+    modalText: document.querySelector("[data-id='modal-text']"),
+    modalBtn: document.querySelector("[data-id='modal-btn']"),
+	
+  },
 
-const menu = document.querySelector('.menu')
-const menuItems = menu.querySelector('.items')
+  state: {
+    moves: [],
+  },
 
-//this will add and remove my 'hidden' class for my items element
-menu.addEventListener('click', (event) => {
-menuItems.classList.toggle('hidden')
-});
+  getGameStatus(moves) {
+    const p1Moves = moves
+      .filter((move) => move.playerId === 1)
+      .map((move) => +move.squareId);
 
+    const p2Moves = moves
+      .filter((move) => move.playerId === 2)
+      .map((move) => +move.squareId);
+
+    const winningPatterns = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7],
+    ];
+
+    let winner = null;
+
+    winningPatterns.forEach((pattern) => {
+      const p1Wins = pattern.every((v) => p1Moves.includes(v));
+      const p2Wins = pattern.every((v) => p2Moves.includes(v));
+
+      if (p1Wins) winner = 1;
+      if (p2Wins) winner = 2;
+    });
+
+    return {
+      status: moves.length === 9 || winner != null ? "complete" : "in-progress", //in progress || complete
+      winner, // 1 || 2 || null
+    };
+  },
+
+  init() {
+    App.registerEvents();
+  },
+
+  registerEvents() {
+    //this will add and remove my 'hidden' class for my items element
+    App.$.menu.addEventListener("click", (event) => {
+      App.$.menuItems.classList.toggle("hidden");
+    });
+
+    App.$.resetBtn.addEventListener("click", (event) => {
+      console.log("Reset the game");
+    });
+
+    App.$.newRoundBtn.addEventListener("click", (event) => {
+      console.log("add new round");
+    });
+
+    App.$.squares.forEach((square) => {
+      square.addEventListener("click", (event) => {
+        //check if there's already a play, if so, return early
+        const hasMove = (squareId) => {
+          const existingMove = App.state.moves.find(
+            (move) => move.squareId === squareId
+          );
+          return existingMove !== undefined;
+        };
+
+        if (hasMove(+square.id)) {
+          return;
+        }
+
+        //determine which player icon to add to square
+        const lastMove = App.state.moves.at(-1);
+        const getOppositePlayer = (playerId) => (playerId === 1 ? 2 : 1);
+        const currentPlayer =
+          App.state.moves.length === 0
+            ? 1
+            : getOppositePlayer(lastMove.playerId);
+
+        const icon = document.createElement("i");
+
+        if (currentPlayer === 1) {
+          icon.classList.add("fa-solid", "fa-x", "turquoise");
+        } else {
+          icon.classList.add("fa-solid", "fa-o", "yellow");
+        }
+
+        App.state.moves.push({
+          squareId: +square.id,
+          playerId: currentPlayer,
+        });
+
+        square.replaceChildren(icon);
+
+        //check if there is a winner or tie game
+        const game = App.getGameStatus(App.state.moves);
+
+        if (game.status === "complete") {
+          App.$.modal.classList.remove('hidden')
+			
+		  let message = ''
+			if (game.winner) {
+				message = `Player ${game.winner} wins`
+          } else {
+			message = `Tie game!`
+
+			App.$.modalText.textContent = message;
+		  }
+        }
+      });
+    });
+  },
+};
+
+window.addEventListener("load", App.init);
